0de5d54d6401c3d17f9bb7e23e979e2a27b7195f
Tanveer Ahmad Khan <tanveerk@mindfiresolutions.com>
2021-12-05 22:15:59 +0530

    added feature to add tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,7 +2,39 @@
 
 # manage tags
 class TagsController < ApplicationController
+  before_action :authenticate_customer!
+
+  # GET /tags
+  # before_action: authenticate_customer!
   def index
     @tags = Tag.all
+  end
+
+  # GET /tags/new
+  # before_action: authenticate_customer!
+  def new
+    @tag = Tag.new
+  end
+
+  # POST /tags
+  # @param name [String]
+  # before_action: authenticate_customer!
+  def create
+    byebug
+    @tag = current_customer.tags.build(tag_params)
+    respond_to do |format|
+      if @tag.save
+        format.html { redirect_to tags_url, notice: 'Tag was successfully created' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+  # only allow a list of trusted parameters through.
+  def tag_params
+    params.require(:tag).permit(:name)
   end
 end
