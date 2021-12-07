270a9096dd676eecdb260ca91c38bfa9a63ab432
Tanveer Ahmad Khan <tanveerk@mindfiresolutions.com>
2021-12-07 18:51:40 +0530

    edit customer profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,9 +4,39 @@
 class ProfilesController < ApplicationController
   before_action :authenticate_customer!
 
-  # GET /profiles
+  # GET /profile
   # before_action :authenticate_customer!
   def show
     @profile = current_customer.profile
+  end
+
+  # GET /profile/edit
+  # before_action :authenticate_customer!
+  def edit
+    @profile = current_customer.profile
+  end
+
+  # PUT /profile
+  # PATCH /profile
+  # @param name [String]
+  # @param name [String]
+  # before_action :authenticate_customer!
+  def update
+    @profile = current_customer.profile
+    @profile.assign_attributes(profile_params)
+    respond_to do |format|
+      if @profile.save
+        format.html { redirect_to profile_url, notice: t('profiles.updated') }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  private
+
+  # only allow a list of trusted parameters through.
+  def profile_params
+    params.require(:profile).permit(:name, :contact)
   end
 end
